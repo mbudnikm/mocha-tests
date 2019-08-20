6a24d317260fbc4cecc245cc4e32127bc7d81ea6
mbudnikm <mbudnikm97@gmail.com>
2019-08-20 13:12:11 +0200

    add nyc coverage

--- mocha/test/calc.spec.js
+++ mocha/test/calc.spec.js
@@ -5,5 +5,11 @@ const { Calculator } = require('../src/calc')
 describe('Calculator', () => {
     it('should add numbers', () => {
         assert.equal(Calculator.add(2, 3), 5)
+        // assert.equal(Calculator.add(2, 3), 6, "błędny wynik dodawania")
+        // assert.equal(Calculator.add(2, 3), 7, "błędny wynik dodawania")
+
+        // if (2 !== 3) {
+        //     throw new Error("2 not equal 3")
+        // }
     })
 })
