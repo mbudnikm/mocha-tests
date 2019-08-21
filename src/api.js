2c886fa4a0ddb6e1c2c4c491dc277b68799a7f63
mbudnikm <mbudnikm97@gmail.com>
2019-08-21 13:50:31 +0200

    add  fetchGeoWithOffices

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 const baseUrl = 'http://localhost:3000'
 
+const chalk = require('chalk')
+
 const fetchGeo = () =>
     fetch(`${baseUrl}/geo`)
         .then(res => res.json())
@@ -24,7 +26,26 @@ const fetchGeoWithOffices = async () => {
     const geo = await geoReq
     const offices = await officesReq
 
+    //console.log(Object.keys(geo), Object.keys(offices))
+
+    const grouped = offices.reduce((acc, o) => {
+        if (!acc[o.country]) {
+          acc[o.country] = []
+        }
+        acc[o.country].push(o)
+        return acc
+      }, {})
     
+      const result = Object.entries(geo)
+        .reduce((acc, [code, country]) => {
+          acc[code] = {
+            country,
+            offices: grouped[country]
+          }
+          return acc
+        }, {})
+      
+      return result
 
 }
 
